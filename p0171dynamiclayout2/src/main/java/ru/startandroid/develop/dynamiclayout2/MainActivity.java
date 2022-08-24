d10b5a6adb0c9724ae2c9a69ea33d2c6693318a2
KVostok <koboll1985@gmail.com>
2022-08-24 16:54:01 +0300

    Add lesson p0171dynamiclayout2

---
+++ p0171dynamiclayout2/src/main/java/ru/startandroid/develop/dynamiclayout2/MainActivity.java
@@ -0,0 +1,75 @@
+package ru.startandroid.develop.dynamiclayout2;
+
+import android.view.Gravity;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout llMain;
+    RadioGroup rgGravity;
+    EditText etName;
+    Button btnCreate;
+    Button btnClear;
+
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        llMain = findViewById(R.id.llMain);
+        rgGravity = findViewById(R.id.rgGravity);
+        etName = findViewById(R.id.etName);
+
+        btnCreate = findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(this);
+
+        btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCreate:
+                // Создание LayoutParams c шириной и высотой по содержимому
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                        wrapContent, wrapContent);
+                // переменная для хранения значения выравнивания
+                // по умолчанию пусть будет LEFT
+                int btnGravity = Gravity.LEFT;
+                // определяем, какой RadioButton "чекнут" и
+                // соответственно заполняем btnGravity
+                switch (rgGravity.getCheckedRadioButtonId()) {
+                    case R.id.rbLeft:
+                        btnGravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter:
+                        btnGravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight:
+                        btnGravity = Gravity.RIGHT;
+                        break;
+                }
+                // переносим полученное значение выравнивания в LayoutParams
+                lParams.gravity = btnGravity;
+
+                // создаем Button, пишем текст и добавляем в LinearLayout
+                Button btnNew = new Button(this);
+                btnNew.setText(etName.getText().toString());
+                llMain.addView(btnNew, lParams);
+
+                break;
+            case R.id.btnClear:
+                llMain.removeAllViews();
+                Toast.makeText(this, "Удалено", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+}
