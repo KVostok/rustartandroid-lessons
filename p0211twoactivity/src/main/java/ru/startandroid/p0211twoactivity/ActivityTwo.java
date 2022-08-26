fba12f2b965b76bcd0c3727e077f0c29cd8b61a8
KVostok <koboll1985@gmail.com>
2022-08-26 16:26:55 +0300

    Add lesson p0211twoactivity

---
+++ p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/ActivityTwo.java
@@ -0,0 +1,13 @@
+package ru.startandroid.p0211twoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
---
+++ p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.startandroid.p0211twoactivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
