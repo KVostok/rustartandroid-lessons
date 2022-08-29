db886045e22e3993de2cc6f1f32a8f8026d5a6f9
KVostok <koboll1985@gmail.com>
2022-08-29 15:52:32 +0300

    Add lesson p0241twoactivitystate

---
+++ p0241twoactivitystate/src/main/java/ru/startandroid/develop/p0241twoactivitystate/ActivityTwo.java
@@ -0,0 +1,53 @@
+package ru.startandroid.develop.p0241twoactivitystate;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    final String TAG = "States";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+        Log.d(TAG, "ActivityTwo: onCreate()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "ActivityTwo: onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "ActivityTwo: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "ActivityTwo: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "ActivityTwo: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "ActivityTwo: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "ActivityTwo: onDestroy()");
+    }
+}
---
+++ p0241twoactivitystate/src/main/java/ru/startandroid/develop/p0241twoactivitystate/MainActivity.java
@@ -0,0 +1,68 @@
+package ru.startandroid.develop.p0241twoactivitystate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    final String TAG = "States";
+
+    Button btnActTwo;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+
+        Log.d(TAG, "MainActivity: onCreate()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "MainActivity: onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "MainActivity: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "MainActivity: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "MainActivity: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "MainActivity: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "MainActivity: onDestroy()");
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ActivityTwo.class);
+        startActivity(intent);
+    }
+}
