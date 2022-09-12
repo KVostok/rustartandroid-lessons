3a2b79befbe5a5c0871f08dd07e079122a32ee3e
KVostok <koboll1985@gmail.com>
2022-09-12 17:23:55 +0300

    Add lesson p0601alertdialogsimple

---
+++ p0601alertdialogsimple/src/main/java/ru/startandroid/develop/p0601alertdialogsimple/MainActivity.java
@@ -0,0 +1,69 @@
+package ru.startandroid.develop.p0601alertdialogsimple;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int DIALOG_EXIT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onclick(View v) {
+        // вызываем диалог
+        showDialog(DIALOG_EXIT);
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        if (id == DIALOG_EXIT) {
+            AlertDialog.Builder adb = new AlertDialog.Builder(this);
+            // заголовок
+            adb.setTitle(R.string.exit);
+            // сообщение
+            adb.setMessage(R.string.save_data);
+            // иконка
+            adb.setIcon(android.R.drawable.ic_dialog_info);
+            // кнопка положительного ответа
+            adb.setPositiveButton(R.string.yes, myClickListener);
+            // кнопка отрицательного ответа
+            adb.setNegativeButton(R.string.no, myClickListener);
+            // кнопка нейтрального ответа
+            adb.setNeutralButton(R.string.cancel, myClickListener);
+            // создаем диалог
+            return adb.create();
+        }
+        return super.onCreateDialog(id);
+    }
+
+   DialogInterface.OnClickListener myClickListener = new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                // положительная кнопка
+                case Dialog.BUTTON_POSITIVE:
+                    saveData();
+                    finish();
+                    break;
+                // негативная кнопка
+                case Dialog.BUTTON_NEGATIVE:
+                    finish();
+                    break;
+                // нейтральная кнопка
+                case Dialog.BUTTON_NEUTRAL:
+                    break;
+            }
+        }
+    };
+
+    void saveData() {
+        Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT).show();
+    }
+}
