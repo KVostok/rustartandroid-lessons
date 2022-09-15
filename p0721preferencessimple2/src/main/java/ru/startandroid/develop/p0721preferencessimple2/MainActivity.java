bdf30393cfec6b6a3daf44d294c463ab0b19241e
KVostok <koboll1985@gmail.com>
2022-09-15 14:06:08 +0300

    Add lesson p0721preferencessimple2

---
+++ p0721preferencessimple2/src/main/java/ru/startandroid/develop/p0721preferencessimple2/MainActivity.java
@@ -0,0 +1,37 @@
+package ru.startandroid.develop.p0721preferencessimple2;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvInfo;
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+
+    protected void onResume() {
+        String listValue = sp.getString("list", "не выбрано");
+        tvInfo.setText("Значение списка - " + listValue);
+        super.onResume();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
