b55e136e590e1ab5284628edf3ca6cd18b3ffae5
KVostok <koboll1985@gmail.com>
2022-09-15 14:35:23 +0300

    Add lesson p0741preferencescode

---
+++ p0741preferencescode/src/main/java/ru/startandroid/develop/p0741preferencescode/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.startandroid.develop.p0741preferencescode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+}
